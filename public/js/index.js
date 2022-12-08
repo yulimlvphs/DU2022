7cd096980af7abda1abb0dcf68f7ebf4487c54fa
yulimlvphs <86701615+yulimlvphs@users.noreply.github.com>
2022-12-08 11:41:13 +0900

    변경사항2

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,16 @@
-const test = 10;
+const userNameElement = document.querySelector('.user-name');
+const headerUserNameElement = document.querySelector('.user');
+const localUserName = localStorage.getItem('userName');
 
-const.log(test);
+const setUserNameInnerHtml = (name) => {
+    userNameElement.innerHTML = `${localStorage.getItem('userName')} <span>님</span>`;
+    headerUserNameElement.innerHTML = `${localStorage.getItem('userName')} <span>님</span>`;
+}
+if(localUserName) {
+    setUserNameInnerHtml(localUserName);
+}
+userNameElement.onclick = () => {
+    const userName = prompt('이름을 입력해 주세요');
+    localStorage.setItem('userName',userName);
+    setUserNameInnerHtml(userName);
+};
