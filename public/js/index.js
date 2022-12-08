32eba2121de5ba1d1d7cc913422bd591ccbfc663
yulimlvphs <86701615+yulimlvphs@users.noreply.github.com>
2022-12-08 16:21:34 +0900

    수정사항end

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,67 @@
-const userNameElement = document.querySelector('.user-name');
+// const userNameElement = document.querySelector('.user-name');
 const headerUserNameElement = document.querySelector('.user');
+const userNameBtn = document.querySelector('#myname');
 const localUserName = localStorage.getItem('userName');
-const userNameBtn = document.querySelector('#name');
-const usernumber = document.querySelector('#user-number');
+const usernumber = document.querySelector('#number');
 const localusernumber = localStorage.getItem('inputUsernumber');
-const userEmail = document.querySelector('#user-email');
-const localuserEmail = localStorage.getItem('inpuUuserEmail');
+const userEmail = document.querySelector('#email');
+const localuserEmail = localStorage.getItem('inputuserEmail');
 
 const setUserNameInnerHtml = (name) => {
-    userNameElement.innerHTML = `${name} <span>님</span>`;
+    headerUserNameElement.innerHTML = `${name} <span>님</span>`;
     userNameBtn.textContent = name;
    // headerUserNameElement.innerHTML = `<span id = 'name'> ${name}</span> <span>님</span>`;
-}
+};
+const setUsernumberInnerHtml = (number) => {
+    usernumber.textContent = number;
+   // headerUserNameElement.innerHTML = `<span id = 'name'> ${name}</span> <span>님</span>`;
+};
+
+const setuserEmailInnerHtml = (email) => {
+    userEmail.textContent = email;
+   // headerUserNameElement.innerHTML = `<span id = 'name'> ${name}</span> <span>님</span>`;
+};
+
 if(localUserName) {
     setUserNameInnerHtml(localUserName);
 }
-userNameElement.onclick = () => {
+if(localusernumber) {
+    setUsernumberInnerHtml(localusernumber);
+}
+if(localuserEmail) {
+    setuserEmailInnerHtml(localuserEmail);
+}
+
+userNameBtn.onclick = () => {
     const userName = prompt('이름을 입력해 주세요');
+   
     localStorage.setItem('userName',userName);
     setUserNameInnerHtml(userName);
 };
 
 usernumber.onclick = () => {
     const inputUsernumber = prompt('학번 입력 -->>');
-    localStorage.setItem('usernumber',inputUsernumber);
-    setUserNameInnerHtml(usernumber);
+    const num = Number(inputUsernumber);
+
+    if (num.length <= 9) {
+      alert('학번은 9자리를 입력하여 주세요.');
+      return false;
+    }
+    localStorage.setItem('inputUsernumber',inputUsernumber);
+    setUsernumberInnerHtml(inputUsernumber);
 };  
+
+userEmail.onclick = () => {
+    const inputuserEmail = prompt('이메일 입력 -->>');
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+var email = document.getElementById('inputuserEmail').value;
+if (email == '' || !re.test($email)) {
+alert("올바른 이메일 주소를 입력하세요")
+return false;
+}
+
+    localStorage.setItem('inputuserEmail',inputuserEmail);
+    setuserEmailInnerHtml(inputuserEmail);
+};  
+
